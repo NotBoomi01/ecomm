cd874027eb71603ba73b5e271a258bcbc086c935
NotBoomi01 <johnmatthew.mayuga@sdca.edu.ph>
2024-10-19 15:59:31 +0800

    hehehe

---
+++ app/config/DatabaseConnect.php
@@ -0,0 +1,35 @@
+<?php
+
+
+class DatabaseConnect{
+
+    private $host = "localhost";
+    private $database = "ecommbatch1";
+    private $dbusername = "root";
+    private $dbpassword = "";
+    private $conn = null;
+
+public function connectDB(){
+   
+    
+        $dsn = "mysql: host=$this->host;dbname=$this->database;";
+        try {
+        $this->conn = new PDO($dsn, $this->dbusername, $this->dbpassword);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $this->conn;
+
+    } catch (Exception $e){
+        echo "Connection Failed: " . $e->getMessage();
+
+        return null;
+    }
+}
+
+
+}
+
+
+
+?>
